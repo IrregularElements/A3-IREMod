856eb4bd3bbacc34dc5101b680d4332deb436ec5
inty <interruptinuse@gmail.com>
2021-09-03 12:10:02 +0000

    Move all debug-enabling macros to main

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -2,8 +2,6 @@
 #define COMPONENT_BEAUTIFIED Main
 #include "\x\ire_mod\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
+#include "\x\ire_mod\addons\main\script_debug_macros.hpp"
 
 #include "\x\ire_mod\addons\main\script_macros.hpp"
